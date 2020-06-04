344d7f82d65cc74743f8cd826ae7c9357f778b77
Spyte <40525440+Spajciuch@users.noreply.github.com>
2020-06-04 22:57:49 +0200

    Just a simple worm xD
    Command that sends a worm img. Don't ask...

---
+++ commands/worm.js
@@ -0,0 +1,9 @@
+module.exports.run = async(client, message, args, embed_color, lang) => {
+message.channel.send({
+  files: ['https://cdn.discordapp.com/attachments/617036048265904129/631152796133228567/shot-cropped-1557063892938.png']
+})
+}
+
+module.exports.help = {
+name: "worm"
+}
