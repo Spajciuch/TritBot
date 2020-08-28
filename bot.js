25a37d9ca07e525ffbe72d8789cca18fea6391fc
Spyte <40525440+Spajciuch@users.noreply.github.com>
2020-08-28 14:02:24 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,14 +28,14 @@ storageBucket: process.env.ID + ".appspot.com",
 messagingSenderId: process.env.SENDER
 };*/
  var fireconfig = {
-apiKey: "AIzaSyCK85kqG_-kKnR0jcWlU9TExiX5QZ74JUE",
-    authDomain: "trit2-eff12.firebaseapp.com",
-    databaseURL: "https://trit2-eff12.firebaseio.com",
-    projectId: "trit2-eff12",
-    storageBucket: "trit2-eff12.appspot.com",
-    messagingSenderId: "874262724144",
-    appId: "1:874262724144:web:c21e79d40b16ea506bf7f5",
-    measurementId: "G-4L0TZNCN08"
+ apiKey: "AIzaSyBewmiPJqsuA9HijzBy3hMDKyVG1zieB6E",
+    authDomain: "chat-9b30f.firebaseapp.com",
+    databaseURL: "https://chat-9b30f.firebaseio.com",
+    projectId: "chat-9b30f",
+    storageBucket: "chat-9b30f.appspot.com",
+    messagingSenderId: "446829660490",
+    appId: "1:446829660490:web:47998d396e1a54391e19b3",
+    measurementId: "G-KSE9KXNZZZ"
 };
 const newDb = require("./database")
 
