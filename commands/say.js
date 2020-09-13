c09355370eb7c6b850b7655cb1149e6f7fad40fa
Spyte <40525440+Spajciuch@users.noreply.github.com>
2020-09-13 22:49:17 +0200

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -2,7 +2,8 @@ const Discord = require("discord.js")
 
 module.exports.run = async (client, message, args, embed_color, lang) => {
     message.delete()
-    message.channel.send(args.join(" "))
+    message.channel.send(args.join(" ").replace("@everyone", "everyone").replace("@here", "here"))
+    if(message.content.includes("@everyone") || message.content.includes("@here") && message.autohr.id == "484006841781583883") message.reply("Skoncz z tym plz")
 }
 
 module.exports.help = {
